ca0131f0af61e711fb01b6b479bf919a9fdc24e4
Alwy <wyalwy.dev@gmail.com>
2021-02-08 09:35:07 +0700

    added login page

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDzpMWZTvmqZ-LDy4blMd7L2GIg8nkkrkk",
-    authDomain: "clone-ab2fb.firebaseapp.com",
-    projectId: "clone-ab2fb",
-    storageBucket: "clone-ab2fb.appspot.com",
-    messagingSenderId: "256654192172",
-    appId: "1:256654192172:web:0d17d84cb37f01232eeb89",
-    measurementId: "G-17QV356DKD"
-  };
+  apiKey: "AIzaSyDzpMWZTvmqZ-LDy4blMd7L2GIg8nkkrkk",
+  authDomain: "clone-ab2fb.firebaseapp.com",
+  projectId: "clone-ab2fb",
+  storageBucket: "clone-ab2fb.appspot.com",
+  messagingSenderId: "256654192172",
+  appId: "1:256654192172:web:0d17d84cb37f01232eeb89",
+  measurementId: "G-17QV356DKD",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
